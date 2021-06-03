6b007b6dd4c1b6356ad3cab56496e7d0cde1ded8
Erzhan Torokulov <erzhan.torokulov@gmail.com>
2021-06-03 11:51:35 +0300

    Update router implementation

---
+++ examples/router.js
@@ -0,0 +1,83 @@
+/* eslint-disable */
+
+function Router() {
+  let listeners = [];
+  let currentPath = location.pathname;
+  let previousPath = null;
+
+  const isMatch = (match, path) =>
+    (match instanceof RegExp && match.test(path)) ||
+    (typeof match === "function" && match(path)) ||
+    (typeof match === "string" && match === path);
+
+  const handleListener = ([_, match, onEnter, onLeave]) => {
+    const args = { currentPath, previousPath, state: history.state };
+
+    isMatch(match, currentPath) && onEnter(args);
+    isMatch(match, previousPath) && onLeave && onLeave(args);
+  };
+
+  const handleAllListeners = () => listeners.forEach(handleListener);
+
+  const generateId = () => {
+    const getRandomNumber = () =>
+      Math.floor(Math.random() * listeners.length * 1000);
+    const doesExist = (id) => listeners.find((listener) => listener.id === id);
+
+    let id = getRandomNumber();
+    while (doesExist(id)) {
+      id = getRandomNumber();
+    }
+    return id;
+  };
+
+  const unsubscribe = (id) => {
+    listeners = listeners.filter((listener) => listener[0] !== id);
+  };
+
+  const on = (match, onEnter, onLeave) => {
+    const id = generateId();
+
+    listeners.push([id, match, onEnter, onLeave]);
+    handleListener([id, match, onEnter], onLeave);
+
+    return () => unsubscribe(id);
+  };
+
+  const go = (url, state) => {
+    previousPath = currentPath;
+    history.pushState(state, url, url);
+    currentPath = location.pathname;
+
+    handleAllListeners();
+  };
+
+  window.addEventListener("popstate", handleAllListeners);
+
+  return { on, go };
+}
+
+const createLogger = (title, attach = true) => (...args) => {
+  console.log(`${title}, args=${JSON.stringify(args)}`);
+  document.getElementById("root").innerHTML = `<h2>${title}</h2>`;
+};
+
+const router = Router();
+const unsubscribeAll = router.on(/.*/, createLogger("/.*"));
+router.on(
+  (pathname) => pathname === "/contacts",
+  createLogger("/contacts"),
+  createLogger("leaving /contacts", false)
+);
+router.on("/about", createLogger("/about"));
+router.on("/about/us", createLogger("/about/us"));
+
+document.body.addEventListener("click", (event) => {
+  if (!event.target.matches("a")) {
+    return;
+  }
+  event.preventDefault();
+  let url = event.target.getAttribute("href");
+  router.go(url);
+  unsubscribeAll();
+});
